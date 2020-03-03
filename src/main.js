03207c61699567f9b7330ea3b7204da9e3817bb4
jiho lee <jiholee@kpu.ac.kr>
2020-03-03 13:39:58 +0900

    views/index.html src/youtube.js
    index.html -> made some layout using bootstrap
    youtube.js -> add find function using ytsr

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
 const { app, BrowserWindow } = require('electron')
-const ytdl = require('ytdl-core')
 
-function createWindow () {
-  // 브라우저 창을 생성합니다.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+function createWindow() {
+    // 브라우저 창을 생성합니다.
+    const win = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        })
+        // and load the index.html of the app.
+    win.loadFile('../views/index.html')
 
-  // and load the index.html of the app.
-  win.loadFile('../views/index.html')
+    // 개발자 도구를 엽니다.
+    win.webContents.openDevTools()
 
-  // 개발자 도구를 엽니다.
-  win.webContents.openDevTools()
+    win.webContents.once('dom-ready', () => {
+        // win.webContents
+        //   .send('sum-request', 23, 98, 3, 61)
+        // ipcMain.once('sum-reply', (event, sum) => {
+        //   doJobWithResult(sum)
+        // })
+    })
 }
 
 // 이 메소드는 Electron의 초기화가 완료되고
@@ -25,19 +31,19 @@ app.whenReady().then(createWindow)
 
 // 모든 윈도우가 닫히면 종료된다.
 app.on('window-all-closed', () => {
-  // macOS에서는 사용자가 명확하게 Cmd + Q를 누르기 전까지는
-  // 애플리케이션이나 메뉴 바가 활성화된 상태로 머물러 있는 것이 일반적입니다.
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // macOS에서는 사용자가 명확하게 Cmd + Q를 누르기 전까지는
+    // 애플리케이션이나 메뉴 바가 활성화된 상태로 머물러 있는 것이 일반적입니다.
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // macOS에서는 dock 아이콘이 클릭되고 다른 윈도우가 열려있지 않았다면
-  // 앱에서 새로운 창을 다시 여는 것이 일반적입니다.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    // macOS에서는 dock 아이콘이 클릭되고 다른 윈도우가 열려있지 않았다면
+    // 앱에서 새로운 창을 다시 여는 것이 일반적입니다.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
 
 // 이 파일에는 나머지 앱의 특정 주요 프로세스 코드를 포함시킬 수 있습니다. 별도의 파일에 추가할 수도 있으며 이 경우 require 구문이 필요합니다.
---
+++ src/youtube.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const ytdl = require('ytdl-core')
+const ytsr = require('ytsr')
+
+/**
+ * Find youtube videos by name. five per page
+ */
+const find = async (name) => {
+  try {
+    let filter = await ytsr.getFilters(name)
+    filter = filter.get('Type').find(o => o.name === 'Video')
+    const options = {
+      limit: 5,
+      nextpageRef: filter.ref
+    }
+    const val = await ytsr(null, options)
+    return val.items
+  } catch (err) {
+    console.log('find failed', err)
+  }
+}
+
+(async () => {
+  console.log(await find('psyqui'))
+})()
