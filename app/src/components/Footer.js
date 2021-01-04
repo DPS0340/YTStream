93c550e68f696a8c81e29f92910501fd94cbc103
jiho lee <optional.int@kakao.com>
2021-01-04 14:12:38 +0900

    Fix components does not return

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,32 +1,38 @@
 import React from 'react'
 
 export function Contributors () {
+  return (
     <div>
       2020 <a href='https://github.com/DPS0340/YTStream/graphs/contributors'>
       Contributors.
       </a>
     </div>
+  )
 }
 
 export function License () {
+  return (
     <a href='https://github.com/DPS0340/YTStream/blob/master/LICENSE' style={{ color: 'inherit', textDecorationColor: 'blue' }}>
-    MIT License
-  </a>
+      MIT License
+    </a>
+  )
 }
 
 export function Contribute () {
-  <div>
-    <a href='https://github.com/DPS0340/YTStream' className='badge badge-secondary'>
-      <span className='h4 lead'>
-        GitHub
-      </span>
-    </a>
-    <a href='https://github.com/DPS0340/YTStream' className='badge badge-primary'>
-      <span className='h4 lead'>
-        Contribute us!
-      </span>
-    </a>
-  </div>
+  return (
+    <div>
+      <a href='https://github.com/DPS0340/YTStream' className='badge badge-secondary'>
+        <span className='h4 lead'>
+          GitHub
+        </span>
+      </a>
+      <a href='https://github.com/DPS0340/YTStream' className='badge badge-primary'>
+        <span className='h4 lead'>
+          Contribute us!
+        </span>
+      </a>
+    </div>
+  )
 }
 
 export default function Footer () {
