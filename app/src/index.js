8f00c175ace13641c9e8f7a5917088ffa567c63c
jiho lee <optional.int@kakao.com>
2021-01-04 12:05:56 +0900

    Refactored Navbar

--- app/src/index.js
+++ app/src/index.js
@@ -1,24 +1,18 @@
 
-window.jQuery = window.$ = require('jquery')
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import Electron, { ipcRenderer } from 'electron'
-import popper from 'popper.js/dist/umd/popper.min'
-import bootstrapjs from 'bootstrap/dist/js/bootstrap.min'
-import bootstrapcss from '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import css from '../main.css'
+import { ipcRenderer } from 'electron'
+window.jQuery = window.$ = require('jquery')
 
 console.log('Hello World!')
 
-Array.prototype.clear = function() {
-  this.splice(0, this.length);
+export const clearArray = function (arr) {
+  arr.splice(0, arr.length)
 }
 
-let musicQueue = {}
+const musicQueue = {}
 let musicCursor = 0
-let currentKeyword = ""
-
-
+let currentKeyword = ''
 
 /**
  * send search keyword with ipcRenderer
@@ -26,18 +20,18 @@ let currentKeyword = ""
 const find = (keyword) => {
   musicCursor = 1
   currentKeyword = keyword
-  if(!musicQueue[keyword]) {
+  if (!musicQueue[keyword]) {
     ipcRenderer.send('youtube-search-perform', keyword)
-    musicQueue[keyword] = [ { query: keyword } ]
+    musicQueue[keyword] = [{ query: keyword }]
   } else {
-    console.log("cached Keyword")
+    console.log('cached Keyword')
     showSearchResult(musicQueue[keyword][0])
   }
 }
 
 const searchBar = () => {
   const val = document.getElementById('Search-Form').value
-  document.getElementById('Search-Form').value = ""
+  document.getElementById('Search-Form').value = ''
   console.log(val)
   if (!val) {
     return
@@ -57,7 +51,7 @@ const hasFront = () => {
 
 const queueFront = (query) => {
   if (hasFront()) {
-    console.log("cached Front")
+    console.log('cached Front')
     showSearchResult(musicQueue[currentKeyword][musicCursor++])
   } else {
     searchQuery(query)
@@ -69,65 +63,63 @@ const hasBack = () => {
 }
 
 const queueBack = () => {
-  if (hasBack()) {
-    console.log("cached Back")
-    console.log(musicQueue[currentKeyword])
-    showSearchResult(musicQueue[currentKeyword][--musicCursor - 1])
-  } else {
+  if (!hasBack()) {
     return
   }
+  console.log('cached Back')
+  console.log(musicQueue[currentKeyword])
+  showSearchResult(musicQueue[currentKeyword][--musicCursor - 1])
 }
 
-
 const showSearchResult = (arg) => {
-  console.log("arg:", arg)
+  console.log('arg:', arg)
   ReactDOM.render(<MusicViewer result={arg}></MusicViewer>,
     document.getElementById('main')
   )
   window.scroll({
-    top: 0, 
-    left: 0, 
-    behavior: 'smooth' 
-   })
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
 }
 
 ipcRenderer.on('youtube-search-result', (event, arg) => {
   showSearchResult(arg)
-  const queryonly = musicQueue[currentKeyword][musicQueue[currentKeyword].length-1]
-  musicQueue[currentKeyword][musicQueue[currentKeyword].length-1] = { ...queryonly, ...arg };
+  const queryonly = musicQueue[currentKeyword][musicQueue[currentKeyword].length - 1]
+  musicQueue[currentKeyword][musicQueue[currentKeyword].length - 1] = { ...queryonly, ...arg }
 })
 
 class MusicThumbnail extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      showPlayer : false
+      showPlayer: false
     }
 
     this.click = this.click.bind(this)
   }
 
   click () {
-    if(this.state.showPlayer === false) {
-      this.setState({ showPlayer : true })
+    if (this.state.showPlayer === false) {
+      this.setState({ showPlayer: true })
     } else {
-      this.setState({ showPlayer : false })
-      
+      this.setState({ showPlayer: false })
     }
   }
+
   render () {
     const e = this.props.elem
-    const replacedLink = e.url.replace("https://www.youtube.com/watch?v=", "")
+    const replacedLink = e.url.replace('https://www.youtube.com/watch?v=', '')
     const downloadUrl = `http://localhost:8890/download/${replacedLink}`
     const downloadMp3Url = `${downloadUrl}.mp3`
-    const thumbnailStyle = {"height": "250px", "width": "100%", "objectFit": "contain", "marginTop": "5px", "marginBottom": "5px"}
+    const thumbnailStyle = { height: '250px', width: '100%', objectFit: 'contain', marginTop: '5px', marginBottom: '5px' }
     const thumbnailUrl = e.bestThumbnail.url
     const watchUrl = `http://localhost:8890/watch/${replacedLink}`
-    const showThumbnailStyle = {"height": "180px", "width": "100%", "objectFit": "contain", "marginTop": "5px", "marginBottom": "5px"}
+    const showThumbnailStyle = { height: '180px', width: '100%', objectFit: 'contain', marginTop: '5px', marginBottom: '5px' }
     if (!this.state.showPlayer) {
       return (
         <div className="row">
-          <div className="col-sm ml-0 pl-0" style={{"height": "250px"}}>
+          <div className="col-sm ml-0 pl-0" style={{ height: '250px' }}>
             <img onClick={this.click} src={thumbnailUrl} className="rounded float-left" style={thumbnailStyle}></img>
           </div>
           <div className="col-sm">
@@ -144,7 +136,7 @@ class MusicThumbnail extends Component {
     } else {
       return (
         <div className="row">
-          <div className="col-sm ml-0 pl-0" style={{"height": "250px"}}>
+          <div className="col-sm ml-0 pl-0" style={{ height: '250px' }}>
             <img src={thumbnailUrl} onClick={this.click} className="rounded float-left" style={showThumbnailStyle}></img>
             <audio controls autoPlay preload="auto" disablePictureInPicture controlsList="nodownload">
               <source src={watchUrl} type="audio/mp3">
@@ -186,10 +178,10 @@ class MusicPrevNext extends Component {
       return (
         <div className="row mb-3">
           <div className="col-sm text-left">
-            <button className="btn btn-success" onClick={() => {queueBack()}}>Previous Page</button>
+            <button className="btn btn-success" onClick={() => { queueBack() }}>Previous Page</button>
           </div>
           <div className="col-sm text-right">
-            <button className="btn btn-primary" onClick={() => {queueFront(nextUrl)}}>Next Page</button>
+            <button className="btn btn-primary" onClick={() => { queueFront(nextUrl) }}>Next Page</button>
           </div>
         </div>
       )
@@ -200,7 +192,7 @@ class MusicPrevNext extends Component {
             <button className="btn btn-dark">Previous Page</button>
           </div>
           <div className="col-sm text-right">
-            <button className="btn btn-primary" onClick={() => {queueFront(nextUrl)}}>Next Page</button>
+            <button className="btn btn-primary" onClick={() => { queueFront(nextUrl) }}>Next Page</button>
           </div>
         </div>
       )
@@ -236,58 +228,56 @@ class Index extends Component {
   }
 }
 
-class Footer extends Component {
-  render () {
-    return (
-      <footer className='footer font-small blue vertical-center text-center fixed-bottom'>
-        <div className='lead'>
-          2020 <a href='https://github.com/DPS0340/YTStream/graphs/contributors'>
-          Contributors.
-          </a><br></br>
-          <a href='https://github.com/DPS0340/YTStream/blob/master/LICENSE' style={{"color": "inherit", "textDecorationColor": "blue"}}>
-            MIT License
-          </a><br></br>
-        </div>
-        <div>
-          <a href='https://github.com/DPS0340/YTStream' className='badge badge-secondary'>
-            <span className='h4 lead'>
-              GitHub
-            </span>
-          </a>
-          <a href='https://github.com/DPS0340/YTStream' className='badge badge-primary'>
-            <span className='h4 lead'>
-              Contribute us!
-            </span>
-          </a><br></br>
-        </div>
-      </footer>
-    )
-  }
+const Footer = () => {
+  return (
+    <footer className='footer font-small blue vertical-center text-center fixed-bottom'>
+      <div className='lead'>
+        2020 <a href='https://github.com/DPS0340/YTStream/graphs/contributors'>
+        Contributors.
+        </a>
+        <br />
+        <a href='https://github.com/DPS0340/YTStream/blob/master/LICENSE' style={{ color: 'inherit', textDecorationColor: 'blue' }}>
+          MIT License
+        </a>
+        <br />
+      </div>
+      <div>
+        <a href='https://github.com/DPS0340/YTStream' className='badge badge-secondary'>
+          <span className='h4 lead'>
+            GitHub
+          </span>
+        </a>
+        <a href='https://github.com/DPS0340/YTStream' className='badge badge-primary'>
+          <span className='h4 lead'>
+            Contribute us!
+          </span>
+        </a><br></br>
+      </div>
+    </footer>
+  )
 }
 
-class Navbar extends Component {
-  handleKeyPress = (event) => {
-    if(event.key === 'Enter'){
+const Navbar = () => {
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
       searchBar()
     }
   }
-  render () {
-    return (
-      <nav className='navbar navbar-dark bg-dark' role='navigation' id='navbar'>
-        <div className='nav navbar-nav navbar-left'>
-          <a className='navbar-brand display-4' href="#" onClick={() => renderIndex()} id='Navbar-Title'>
-            {this.props.title}
-          </a>
+  return (
+    <nav className='navbar navbar-dark bg-dark' role='navigation' id='navbar'>
+      <div className='nav navbar-nav navbar-left'>
+        <a className='navbar-brand display-4' href="#" onClick={() => renderIndex()} id='Navbar-Title'>
+          {this.props.title}
+        </a>
+      </div>
+      <div className='nav navbar-nav navbar-right'>
+        <div className="form-inline">
+          <input className="form-control bg-dark text-white lead border border-white mr-1" onKeyPress={handleKeyPress} type="search" placeholder="Search Youtube..." aria-label="Search" id="Search-Form" />
+          <button className="btn btn-primary text-white lead" onClick={() => searchBar()}>Search!</button>
         </div>
-        <div className='nav navbar-nav navbar-right'>
-          <div className="form-inline">
-            <input className="form-control bg-dark text-white lead border border-white mr-1" onKeyPress={this.handleKeyPress} type="search" placeholder="Search Youtube..." aria-label="Search" id="Search-Form" />
-            <button className="btn btn-primary text-white lead" onClick={() => searchBar()}>Search!</button>
-          </div>
-        </div>
-      </nav>
-    )
-  }
+      </div>
+    </nav>
+  )
 }
 
 class Main extends Component {
